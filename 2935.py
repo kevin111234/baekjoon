0d4433856044cf9ac4979a4b9e5bdf89d241b856
woghd111234 <jaehong243587@gmail.com>
2024-11-08 15:39:57 +0900

    2935

---
+++ 2935.py
@@ -0,0 +1,12 @@
+num1 = int(input())
+op = input()
+num2 = int(input())
+if op == "+":
+  result = num1+num2
+elif op == "-":
+  result = num1-num2
+elif op == "*":
+  result = num1*num2
+elif op == "/":
+  result = num1/num2
+print(result)
