a757acf5632eaa280f2d02f71b958ac4afb6c9a6
kevin111234 <woghd111234@naver.com>
2024-12-11 14:20:19 +0900

    1002

--- 1002.py
+++ 1002.py
@@ -1,12 +1,15 @@
-n = int(input())
-output = []
+import math
+
+n=int(input())
+
 for i in range(n):
-  a = input()
-  b = a.split(" ")
-  result = ""
-  for k in range(len(b[1])):
-    for j in range(int(b[0])):
-      result += b[1][k]
-  output.append(result)
-for l in range(n):
-  print(output[l])
+    x1, y1, r1, x2, y2, r2 = map(int, input().split(" "))
+    d = math.sqrt((x1-x2)**2 + (y1-y2)**2)
+    if d == 0 and r1==r2:
+        print(-1)
+    elif abs(r1-r2) == d or r1+r2==d:
+        print(1)
+    elif abs(r1-r2) < d < r1+r2:
+        print(2)
+    else:
+        print(0)
