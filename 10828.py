4541a7c3b363bf270fe961d89c17ea7a0ac8034b
kevin111234 <woghd111234@naver.com>
2024-12-14 15:44:27 +0900

    10828

---
+++ 10828.py
@@ -0,0 +1,25 @@
+n = int(sys.stdin.readline())
+queue = []
+
+for i in range(n):
+    cmd = sys.stdin.readline().split()
+    if cmd[0] == "push":
+        queue.append(int(cmd[1]))
+    elif cmd[0] == "pop":
+        if len(queue) > 0:
+            print(queue[-1])
+            queue.pop(-1)
+        else:
+            print(-1)
+    elif cmd[0] == "size":
+        print(len(queue))
+    elif cmd[0] == "empty":
+        if len(queue) == 0:
+            print(1)
+        else:
+            print(0)
+    elif cmd[0] == "top":
+        if len(queue) > 0:
+            print(queue[-1])
+        else:
+            print(-1)
